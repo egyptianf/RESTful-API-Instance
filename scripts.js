df6b22066179e212739458184a94b4181a7a8258
nerdo <walieda74@gmail.com>
2019-02-25 02:10:55 +0200

    Got the parameters of the API, tested it. The only remaining thing is passing its value to the third API, which is supposed to be Facebook API. I only used one service from here.

--- scripts.js
+++ scripts.js
@@ -2,19 +2,17 @@ var fact;
 
 function mathfact() {
   var num = document.getElementById("num").value;
-  window.alert(num);
-  window.alert('http://numbersapi.com/' + num + '/math?notfound=floor&fragment');
   $.get('http://numbersapi.com/' + num + '/math?notfound=floor&fragment', function (data) {
     fact = data;
     $('#number').text(data);
-    $('#number').append('<a href="" onclick="add your function here">  get result</a>');
+    $('#number').append('<a href="" onclick="bingSearch();"> Get a result!</a>');
   });
 }
 
 function randomfact() {
   $.get('http://numbersapi.com/random/trivia', function (data) {
     $('#number').text(data);
-    $('#number').append('<a href="" onclick="add your function here"> get result</a>');
+    $('#number').append('<a href="" onclick="bingSearch();"> Get a result!</a>');
     fact = data;
   });
 }
@@ -22,20 +20,32 @@ function randomfact() {
 
 
 /*
- * Code for Bing Web Search API
+ * Code for Google Custom Web Search API
  * Author egyptianf
  */
-var url = 'http://api.openweathermap.org/data/2.5/weather?q=London&APPID=70c238bc7688af78c117e3f64df92ba7';
-
 //This only works in debugging mode! I can't figure out why!!
 //Update: For God's Sake! There was a form in HTML that when I removed the code just worked!
 function bingSearch() {
   alert('We have stepped inside BingSearch().');
-  $.getJSON(url, function (result) {
-    $(".parag1").append(result.coord.lon);
-    console.log("hhaha");
-    console.log(result);
+  var realurl = 'https://www.googleapis.com/customsearch/v1?';
+  var key = 'AIzaSyDmptAWny2K5sffaNe5jKts2DF6ZgTBs2g';
+  var customSearchEngine = '017505848282137967180:eexzoloxdao';
+  var apiQuery = realurl + "&key=" + key + "&cx=" + customSearchEngine + "&q=" + fact;
+
+  var searchResult;
+
+  $.get(apiQuery, function (result) {
+    searchResult = result.items[0].link;
+    $('#number').text(result.items[0].link);
+    $('#number').append(result.items[0].link);
+  }, 'json');
+
+  /*   $.getJSON(realurl, function (result) {
+      $('#number').append(result.totalCount);
+      console.log("hhaha");
+      console.log(result);
 
 
-  });
+    });*/
+  return searchResult;
 }
