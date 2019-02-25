f6178c225d405e7d5720a55b047b6ff451985ad5
nerdo <walieda74@gmail.com>
2019-02-25 23:26:40 +0200

    Removed index.html and integrated the code in project.html and scripts.js.

--- scripts.js
+++ scripts.js
@@ -37,3 +37,56 @@ function bingSearch() {
 
   });
 }
+
+
+
+/*
+ * Code for Facebook API.
+ * Permission denied from Facebook.
+ * Author: ahmedkamalmohamed
+ */
+
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: '2230988630507670',
+    cookie: true,
+    xfbml: true,
+    version: 'v3.2'
+  });
+  FB.getLoginStatus(function (response) {
+    if (response.status === 'connected') {
+      document.getElementById('status').innerHTML = 'We are connected.';
+      document.getElementById('login').style.visibility = 'hidden';
+    } else if (response.status === 'not_authorized') {
+      document.getElementById('status').innerHTML = 'We are not logged in.'
+    } else {
+      document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
+    }
+  });
+};
+(function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) { return; }
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+function login() {
+  FB.login(function (response) {
+    if (response.status === 'connected') {
+      document.getElementById('status').innerHTML = 'We are connected.';
+      document.getElementById('login').style.visibility = 'hidden';
+    } else if (response.status === 'not_authorized') {
+      document.getElementById('status').innerHTML = 'We are not logged in.'
+    } else {
+      document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
+    }
+  }, { scope: 'publish_actions' });
+}
+
+
+function post() {
+  FB.api('/me/feed', 'post', { message: }, function (response) {
+    document.getElementById('status').innerHTML = response.id;
+  });
+}
